427dd6a948fb89e0147c6f1412f4cc509821080f
aluno <aluno@NV-LAB02-135505.ifms.edu.br>
2024-03-11 11:22:38 -0400

    Meu primeiro comit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css"
+function App() {
+  let nome = "Ai bolso..."
+  let sobrenome = "....naro..........goz.........bo.....naro"
+  return (
+    <>
+      <h1 className="nome">{nome}</h1>
+      <p className="sobrenome">{sobrenome}</p>
+    </>
+  )// Retorna JSX - JavaScript XML
+}
+export default App
