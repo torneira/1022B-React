da37058c6c2bafd11f27dc384efef0425d57a655
torneira <gustavo.prazer@estudante.ifms.edu.br>
2024-03-13 10:15:06 -0400

    Meu primeiro componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css"
+import MeuComponente from "./componentes/MeuComponente"
 function App() {
   let nome = "Ai bolso..."
   let sobrenome = "....naro..........goz.........bo.....naro"
   return (
     <>
+      <MeuComponente></MeuComponente>
       <h1 className="nome">{nome}</h1>
       <p className="sobrenome">{sobrenome}</p>
     </>
---
+++ src/componentes/MeuComponente.tsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import "./MeuComponente.css"
+//Criar primeiro componente
+//Esse componente será o componente do nosso header
+//No header temos 300px de altura ele é do tamanho da tela 
+//Ele tem uma borda amarela de 2px solida
+//Dentro da tag header nos temos uma div com o texto "logo" de tamanho 200x200px centralizada na vertical a 30px da lateral esquerda 
+function MeuComponente(){
+    return (
+        <header>
+            <div></div>
+        </header>
+    )
+}
+
+export default MeuComponente;
